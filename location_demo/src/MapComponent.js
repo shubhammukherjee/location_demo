e57709d401b680e6351cafdefe2de7ff08f09345
shubhammukherjee <53419676+shubhammukherjee@users.noreply.github.com>
2021-05-19 13:02:44 +0530

    Update MapComponent.js

--- location_demo/src/MapComponent.js
+++ location_demo/src/MapComponent.js
@@ -1,5 +1,8 @@
-import { GoogleMap, Marker } from "react-google-maps"
 
 const MyMapComponent = (props) =>
  <iframe src={`http://maps.google.com/maps?q=${props.lat},${props.long}&z=16&output=embed`} height="450" width="600"></iframe>
-export default MyMapComponent;
+
+ export default MyMapComponent;
+
+
+
